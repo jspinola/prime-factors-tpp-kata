35c9403a46d5ab4114627576855c6a71d89b9374
Jesús Spínola <jesus.spinola@lifullconnect.com>
2021-02-27 22:51:14 +0100

    Revert to node modules and rename

--- src/dummy.test.js
+++
@@ -1,7 +0,0 @@
-import dummy from './dummy'
-
-describe('dummy', () => {
-  it('returns true', () => {
-    expect(dummy()).toBe(true)
-  })
-})
---
+++ src/factorize.test.js
@@ -0,0 +1,7 @@
+const factorize = require('./factorize')
+
+describe('factorize should', () => {
+  it('return true', () => {
+    expect(factorize()).toBe(true)
+  })
+})
