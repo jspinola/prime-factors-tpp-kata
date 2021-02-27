d77d4e42321f56754b46079bcc54d4b11b6c5e82
Jesús Spínola <jesus.spinola@lifullconnect.com>
2021-02-28 00:10:39 +0100

    Add test case

--- src/factorize.test.js
+++ src/factorize.test.js
@@ -8,7 +8,8 @@ describe('factorize should', () => {
     [6, [2,3]],
     [9, [3,3]],
     [12, [2,2,3]],
-    [15, [3,5]]
+    [15, [3,5]],
+    [18, [2,3,3]]
   ])
   ('return factor of %i', (number, expectedResult) => {
     expect(factorize(number)).toEqual(expectedResult)
