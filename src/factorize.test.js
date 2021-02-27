03b8c40928f541e01e5b1d5b010ecdc20dc78673
Jesús Spínola <jesus.spinola@lifullconnect.com>
2021-02-27 23:43:57 +0100

    Simplify tests structure

--- src/factorize.test.js
+++ src/factorize.test.js
@@ -1,19 +1,13 @@
 const factorize = require('./factorize')
 
 describe('factorize should', () => {
-  it('return factor of 2', () => {
-    expect(factorize(2)).toEqual([2])
-  })
-
-  it('return factor of 3', () => {
-    expect(factorize(3)).toEqual([3])
-  })
-
-  it('return factor of 4', () => {
-    expect(factorize(4)).toEqual([2, 2])
-  })
-
-  it('return factor of 6', () => {
-    expect(factorize(6)).toEqual([2, 3])
+  test.each([
+    [2, [2]],
+    [3, [3]],
+    [4, [2,2]],
+    [6, [2,3]],
+  ])
+  ('return factor of %i', (number, expectedResult) => {
+    expect(factorize(number)).toEqual(expectedResult)
   })
 })
