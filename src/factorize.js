0f52996a3bd4373f191db5e548c9d4f04b0e1acc
Jesús Spínola <jesus.spinola@lifullconnect.com>
2021-02-27 22:55:14 +0100

    Null to constant

--- src/factorize.js
+++ src/factorize.js
@@ -1,3 +1,3 @@
-module.exports = () => {
-  return true
+module.exports = (number) => {
+  return [2]
 }
--- src/factorize.test.js
+++ src/factorize.test.js
@@ -1,7 +1,7 @@
 const factorize = require('./factorize')
 
 describe('factorize should', () => {
-  it('return true', () => {
-    expect(factorize()).toBe(true)
+  it('return factor of 2', () => {
+    expect(factorize(2)).toEqual([2])
   })
 })
