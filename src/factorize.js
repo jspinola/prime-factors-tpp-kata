513c4cfb2f7954a9992e178228cad43d1173344f
Jesús Spínola <jesus.spinola@lifullconnect.com>
2021-02-27 23:52:36 +0100

    Refactor, use array

--- src/factorize.js
+++ src/factorize.js
@@ -1,16 +1,18 @@
 
+const primeNumbers = [2, 3]
+
 const factorize = (number) => {
   if (number < 4) {
     return [number]
   }
 
   let divider;
-  if (number % 2 === 0) {
-    divider = 2
+  if (number % primeNumbers[0] === 0) {
+    divider = primeNumbers[0]
   }
 
-  if (number % 3 === 0) {
-    divider = 3
+  if (number % primeNumbers[1] === 0) {
+    divider = primeNumbers[1]
   }
   const result = factorize(number / divider)
   result.push(divider)
